71bbde275c9ce2925c3b70c1b69342330c94dc73
Mashnin Egor <mashnin_yegor@mail.ru>
2024-11-27 22:18:29 +0300

    Initial Commit

---
+++ ArcaneCharactersSwiftUI/ArcaneCharactersSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArcaneCharactersSwiftUIApp.swift
+//  ArcaneCharactersSwiftUI
+//
+//  Created by Yoji on 27.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct ArcaneCharactersSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
